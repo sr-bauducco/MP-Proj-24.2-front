d55b7e414fd999befd66ec5af5f3cad28db5e039
EduBarb0sa <edus.barbosa16@gmail.com>
2025-02-12 03:00:03 -0300

    arrumando o app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/login';
 import Register from './Components/Register/register'; 
-import ProductFeed from './Components/ProductFeed/ProductFeed'; 
+import ProductFeed from './Components/ProductFeed/ProductFeed';
+import RegisterProduct from './Components/RegisterProduto/register-product';
+import RegisterFeira from './Components/RegisterFeira/register-feira'; 
 import './App.css';
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/feed" element={<ProductFeed />} />
+          <Route path="/register-product" element={<RegisterProduct />} />
+          <Route path="/register-feira" element={<RegisterFeira />} />
         </Routes>
       </div>
     </BrowserRouter>
