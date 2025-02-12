94eebb8abe7e0ab4106de1474e471211d3416fc1
EduBarb0sa <edus.barbosa16@gmail.com>
2025-02-12 02:40:15 -0300

    tela de cadastro de feira e produto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './Components/Login/login';
 import Register from './Components/Register/register'; // Assumindo que este é o caminho do seu componente Register
+import RegisterProduct from './Components/RegisterProduto/register-product'
+import RegisterFeira from './Components/RegisterFeira/register-feira'
 import './App.css';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/register-product" element={<RegisterProduct />} />
+          <Route path="/register-feira" element={<RegisterFeira />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/Components/RegisterFeira/register-feira.jsx
@@ -0,0 +1,31 @@
+import './register-feira'
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  return (
+    <div className="container">
+      <form>
+        <h1>Registre sua Feira!</h1>
+        <div className="input-field">
+          <input className="card" type='text' placeholder='Nome'/>
+        </div>
+        <div className="input-field">
+          <input className="card" type='text' placeholder='Descrição'/>
+        </div>
+        <div className="input-field">
+          <input className="card" type='text' placeholder='Localização'/>
+        </div>
+
+        <button>Cadastrar Feira</button>
+
+        <div className="signup-link">
+          <p>
+            Quer voltar para a pagina princial? <Link to="/">Clique Aqui</Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Register;
