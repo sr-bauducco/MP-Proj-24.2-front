99248c4075bb5a8a008dd3350b2c72dcd9750972
EduBarb0sa <edus.barbosa16@gmail.com>
2025-02-12 04:57:26 -0300

    header e botão para perfil

--- src/Components/ProductFeed/ProductFeed.jsx
+++ src/Components/ProductFeed/ProductFeed.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./ProductFeed.css";
 import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 const Modal = ({ isOpen, onClose }) => {
   if (!isOpen) return null; // Não renderiza o modal se isOpen for false
@@ -72,61 +73,69 @@ const ProductFeed = () => {
   };
 
   return (
-    <div className="feed-container">
-      <header className="feed-header">
-      <div>
-      <h1>Produtos da Feira</h1>
-      <div className="header-actions">
-        <input
-          type="search"
-          placeholder="Buscar produtos..."
-          className="search-input"
-        />
-        <button className="filter-button" onClick={toggleOptions}>
-          Filtrar
-        </button>
-
-        {showOptions && (
-          <ul className="options-list">
-            <li onClick={() => handleOptionClick('Feiras')}>Feiras</li>
-            <li onClick={() => handleOptionClick('Produtos')}>Produtos</li>
-            <li onClick={() => handleOptionClick('Produtos')}>Banca</li>
-            <li onClick={() => handleOptionClick('Tudo')}>Tudo</li>
-          </ul>
-        )}
-
-        {selectedOption && <p>Opção selecionada: {selectedOption}</p>}
+    <>
+      <div className="container2">
+        <header className="header2">
+          <button className="profile-btn"><Link to='/usuario' className="mudacor"> Ir para o Perfil</Link></button>
+        </header>
       </div>
-    </div>
-      </header>
 
-      <div className="products-grid">
-        {products.map((product) => (
-          <div key={product.id} className="product-card">
-            <img 
-              src={product.image} 
-              alt={product.name} 
-              className="product-image"
-            />
-            <div className="product-info">
-              <h3>{product.name}</h3>
-              <p className="seller-name">Vendedor: {product.seller}</p>
-              <p className="product-description">{product.description}</p>
-              <p className="product-price">
-                R$ {product.price.toFixed(2)}
-              </p>
-              <div className="botoes">
-                <button className="contact-button">
-                  Contactar Vendedor
-                </button>
-                <button className="contact-button" onClick={handleButtonClick}>Faça uma avaliação</button>
-                  <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
+      <div className="feed-container">
+        <header className="feed-header">
+        <div>
+        <h1>Produtos da Feira</h1>
+        <div className="header-actions">
+          <input
+            type="search"
+            placeholder="Buscar produtos..."
+            className="search-input"
+          />
+          <button className="filter-button" onClick={toggleOptions}>
+            Filtrar
+          </button>
+
+          {showOptions && (
+            <ul className="options-list">
+              <li onClick={() => handleOptionClick('Feiras')}>Feiras</li>
+              <li onClick={() => handleOptionClick('Produtos')}>Produtos</li>
+              <li onClick={() => handleOptionClick('Produtos')}>Banca</li>
+              <li onClick={() => handleOptionClick('Tudo')}>Tudo</li>
+            </ul>
+          )}
+
+          {selectedOption && <p>Opção selecionada: {selectedOption}</p>}
+        </div>
+      </div>
+        </header>
+
+        <div className="products-grid">
+          {products.map((product) => (
+            <div key={product.id} className="product-card">
+              <img 
+                src={product.image} 
+                alt={product.name} 
+                className="product-image"
+              />
+              <div className="product-info">
+                <h3>{product.name}</h3>
+                <p className="seller-name">Vendedor: {product.seller}</p>
+                <p className="product-description">{product.description}</p>
+                <p className="product-price">
+                  R$ {product.price.toFixed(2)}
+                </p>
+                <div className="botoes">
+                  <button className="contact-button">
+                    Contactar Vendedor
+                  </button>
+                  <button className="contact-button" onClick={handleButtonClick}>Faça uma avaliação</button>
+                    <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
